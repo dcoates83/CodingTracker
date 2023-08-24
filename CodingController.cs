930b3135198f0c27a70ec3bef76732415de255e9
Devon <devon.coates@totaleto.com>
2023-08-24 16:47:18 -0600

    Setup initial files

---
+++ CodingController.cs
@@ -0,0 +1,34 @@
+﻿namespace CodingTracker
+{
+    internal class CodingController
+    {
+        public static void ParseUserInput(string input)
+        {
+            switch (input)
+            {
+                case "1":
+                    break;
+                case "2":
+                    break;
+                case "3":
+                    break;
+                case "4":
+                    break;
+                case "5":
+                    break;
+                case "6":
+                    break;
+                default:
+                    if (string.IsNullOrEmpty(input))
+                    {
+                        Console.WriteLine("Invalid Input, please type a response");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Please select a value in the list");
+                    }
+                    break;
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,7 +4,31 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var _running = true;
+            while (_running)
+            {
+                UserInput.PromptUser();
+                var resp = UserInput.UserResponse();
+
+                if (resp != null && resp == "0")
+                {
+                    _running = false;
+                }
+
+                Console.WriteLine(resp);
+            }
+            //var test = ConfigurationManager.ConnectionStrings[1].ConnectionString;
+
+
+
+            // create a table
+            // modal that maps the database entities
+
+            // class that deals with validation?
+            // class that handles the SQL connections
+            // class that calculates the coding session
         }
+
     }
+
 }
