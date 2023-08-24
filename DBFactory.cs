777894c47328aa8f766e5df2a0e10436a974a09c
Devon <devon.coates@totaleto.com>
2023-08-24 17:44:37 -0600

    added creating of db

---
+++ DBFactory.cs
@@ -0,0 +1,34 @@
+﻿using System.Data.SQLite;
+
+namespace CodingTracker
+{
+    internal class DBFactory
+    {
+        public SQLiteConnection CreateConnection(string connectionString)
+        {
+            using (var conn = new SQLiteConnection(connectionString))
+            {
+                conn.Open();
+                return conn;
+            }
+        }
+        public static void CreateDatabase(string connectionString)
+        {
+            var conn = new DBFactory().CreateConnection(connectionString);
+            var tblCommand = conn.CreateCommand();
+            tblCommand.CommandText = @"
+CREATE TABLE IF NOT EXISTS CodingSession (
+    ID        INT  NOT NULL,
+    StartTime TIME ,
+    EndTime   TIME ,
+    Duration  TIME ,
+    PRIMARY KEY (
+        ID
+    )
+);
+";
+            tblCommand.ExecuteNonQuery();
+            conn.Close();
+        }
+    }
+}
