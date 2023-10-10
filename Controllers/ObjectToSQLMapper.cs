3ab33e6b22cb1284b19f0c8977fdc416db2b88b5
Devon <devon.coates@totaleto.com>
2023-10-10 14:52:27 -0600

    fix id 0 bug

--- Controllers/ObjectToSQLMapper.cs
+++ Controllers/ObjectToSQLMapper.cs
@@ -24,14 +24,7 @@ namespace CodingTracker.Controllers
 
                 var value = property.GetValue(codingSession);
 
-                // because id can be 0 when unset it always defaults to 0
-                // this causes the bug where you can't save and item with a id of 0 though
-
-                if (value != null && value.GetType() == typeof(int) && (int)value == 0)
-                {
-                    continue;
-                }
-                else if (value != null)
+                if (value != null)
                 {
 
                     columns += $"'{property.Name}',";
--- Models/CodingSessionModel.cs
+++ Models/CodingSessionModel.cs
@@ -2,7 +2,7 @@
 {
     public class CodingSessionModel
     {
-        public int Id { get; set; }
+        public int? Id { get; set; }
         public DateTime? StartTime { get; set; }
         public DateTime? EndTime { get; set; }
         public TimeSpan? Duration { get; set; }
